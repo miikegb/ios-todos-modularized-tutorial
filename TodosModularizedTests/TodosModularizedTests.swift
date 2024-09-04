2e3a4873419ab559b53c44a526ab4e3dda318196
Mike Gonzalez <mikegb.dev@gmail.com>
2024-09-03 21:32:45 -0500

    Initial Commit

---
+++ TodosModularizedTests/TodosModularizedTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodosModularizedTests.swift
+//  TodosModularizedTests
+//
+//  Created by Miguel Gonzalez on 9/3/24.
+//
+
+import Testing
+@testable import TodosModularized
+
+struct TodosModularizedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
